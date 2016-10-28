653a82f90997c69227a166d6a5c8a41e9aa8b7b7
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-10-28 09:54:20 +0200

    uber-basic plugin definition

---
+++ ev7l-events/ev7l-events.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Ecovillage 7 Linden Event System
+ *
+ * @package   EV7L_Events
+ * @license   AGPL-3.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: 7L Events
+ * Plugin URI:  https://github.com/ecovillage/ev7l-events
+ * Description: Enables post types 'Event', 'Event Category' and 'Referee',
+ *              provides functionality like widgets to be used in views.
+ * Version:     0.0.1
+ * Author:      Felix Wolfsteller
+ * Author URI:  https://econya.de
+ * Text Domain: 7l-event-post-type
+ * License:     AGPL-3.0+
+ * License URI: http://www.gnu.org/licenses/agpl-3.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+  die;
+}
+

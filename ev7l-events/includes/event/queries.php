2ad3049bbf7a5ee81222fe6a826a7c4e3d322b67
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-12-11 15:06:10 +0100

    add some comments to custom php query functions

--- ev7l-events/includes/event/queries.php
+++ ev7l-events/includes/event/queries.php
@@ -20,6 +20,11 @@ function upcoming_events() {
   return $events;
 }
 
+/**
+ * Queries past events
+ *
+ * @since 0.0.1
+ */
 function past_events() {
   $events = new WP_Query( array(
     'post_type' => 'ev7l-event',
@@ -36,6 +41,11 @@ function past_events() {
   return $events;
 }
 
+/**
+ * Queries events happening in a given year
+ *
+ * @since 0.0.1
+ */
 function events_in_year($eventyear) {
   $events = new WP_Query( array(
     'post_type' => 'ev7l-event',
@@ -57,6 +67,11 @@ function events_in_year($eventyear) {
   return $events;
 }
 
+/**
+ * Queries events happening in the given month of a given year
+ *
+ * @since 0.0.1
+ */
 function events_in_year_month($eventyear, $eventmonth) {
   $month = str_pad($eventmonth, 2, '0', STR_PAD_LEFT);
   $events = new WP_Query( array(
@@ -79,6 +94,14 @@ function events_in_year_month($eventyear, $eventmonth) {
   return $events;
 }
 
+/**
+ * Queries events of a category, either before or after NOW.
+ *
+ * timecomp is either "<=" (before NOW) or ">=" (after NOW).
+ *
+ * Use the friendlier upcoming_events_in_category and past_events_in_category to avoid setting timecomp yourself (and make your code more readable).
+ * @since 0.0.1
+ */
 function events_in_category_op($category_post_id, $timecomp) {
   $events = new WP_Query( array(
     'post_type'  => 'ev7l-event',
@@ -102,14 +125,32 @@ function events_in_category_op($category_post_id, $timecomp) {
   return $events;
 }
 
+/**
+ * Queries upcoming events of a category
+ *
+ * @since 0.0.1
+ */
 function upcoming_events_in_category($category_post_id) {
   return events_in_category_op($category_post_id, '>=');
 }
 
+/**
+ * Queries past events of a category
+ *
+ * @since 0.0.1
+ */
 function past_events_in_category($category_post_id) {
   return events_in_category_op($category_post_id, '<=');
 }
 
+/**
+ * Queries events of a referee, either before or after NOW.
+ *
+ * timecomp is either "<=" (before NOW) or ">=" (after NOW).
+ *
+ * Use the friendlier upcoming_events_by_referee and past_events_by_referee to avoid setting timecomp yourself (and make your code more readable).
+ * @since 0.0.1
+ */
 function events_by_referee_op($referee_post_id, $timecomp) {
   $events = new WP_Query( array(
     'post_type'  => 'ev7l-event',
@@ -133,14 +174,29 @@ function events_by_referee_op($referee_post_id, $timecomp) {
   return $events;
 }
 
+/**
+ * Queries upcoming events of a referee
+ *
+ * @since 0.0.1
+ */
 function upcoming_events_by_referee($referee_post_id) {
   return events_by_referee_op($referee_post_id, '>=');
 }
 
+/**
+ * Queries past events of a referee
+ *
+ * @since 0.0.1
+ */
 function past_events_by_referee($referee_post_id) {
   return events_by_referee_op($referee_post_id, '<=');
 }
 
+/**
+ * Queries referees of a given event
+ *
+ * @since 0.0.1
+ */
 function referees_by_event($event_post_id) {
   $referee_post_ids = get_post_meta($event_post_id, 'referee_id', false);
   // Needs to be guarded (when nil return for post__in get_post_meta)

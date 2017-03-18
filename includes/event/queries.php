40bdba6e10a1f7fef79cf4ec06bc43ef928fb8cc
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2017-03-18 14:58:54 +0100

    add limit to upcoming_events query

--- includes/event/queries.php
+++ includes/event/queries.php
@@ -4,7 +4,7 @@
  *
  * @since 0.0.1
  */
-function upcoming_events() {
+function upcoming_events($limit = 20) {
   $events = new WP_Query( array(
     'post_type' => 'ev7l-event',
     'meta_query' => array(
@@ -16,7 +16,8 @@ function upcoming_events() {
     ),
     'order' => 'ASC',
     'orderby' => 'meta_value',
-    'nopaging' => true) );
+    'posts_per_page' => $limit
+    ) );
   return $events;
 }
 
